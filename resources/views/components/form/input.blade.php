534d77481468dead3bc93d881e1cce5f1643d130
Isumairuu <ismail.elfekak@gmail.com>
2021-09-30 21:27:38 +0100

    Updated the form fields with blade components

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,15 +1,11 @@
 @props(['name','type'=>'text'])
-<div class="my-5">
-    <label for="{{$name}}" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-        {{ucwords($name)}}
-    </label>
+<x-form.field>
+    <x-form.label name="{{$name}}"/>
     <input class="border border-gray-400 p-2 w-full rounded"
            type="{{$type}}"
            name="{{$name}}"
            id="{{$name}}"
            value="{{old($name)}}"
            required>
-    @error($name)
-    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-    @enderror
-</div>
+    <x-form.error name="{{$name}}"/>
+</x-form.field>
