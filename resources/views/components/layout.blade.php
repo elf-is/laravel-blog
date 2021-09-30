cbcf512a3c943596e7b17cfef7cbe23fae98562d
Isumairuu <ismail.elfekak@gmail.com>
2021-09-30 21:42:20 +0100

    Added a dropdown menu for admin access

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,11 +32,17 @@
 
         <div class="mt-8 md:mt-0 flex items-center">
             @auth
-                <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                    </x-slot>
+                    <x-dropdown-item href="/admin/dashboard">Dashboard</x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts/create">New Post</x-dropdown-item>
+                </x-dropdown>
 
                 <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-4">
                     @csrf
-
                     <button type="submit">Log Out</button>
                 </form>
             @else
