2305c815ed58d48f7a3fd413375e8db5057f24c5
Isumairuu <ismail.elfekak@gmail.com>
2021-10-09 23:05:28 +0100

    Update posts.php
    Added routes names

--- routes/posts.php
+++ routes/posts.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('show_post');
+Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store'])->name('comment.store');
 
-Route::delete('posts/{post:slug}/comments/delete/{comment:id}', [PostCommentsController::class, 'destroy']);
+Route::delete('posts/{post:slug}/comments/delete/{comment:id}', [PostCommentsController::class, 'destroy'])->name('comment.destroy');
 
-Route::patch('posts/{post:slug}/comments/edit/{comment:id}', [PostCommentsController::class, 'update']);
+Route::patch('posts/{post:slug}/comments/edit/{comment:id}', [PostCommentsController::class, 'update'])->name('comment.update');
